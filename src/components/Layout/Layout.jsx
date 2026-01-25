ef595b026241c34411dd65cab82a72bd3cf60648
fantadiallo <fatmbo0003@outlook.com>
2026-01-25 18:48:23 +0100

    refactor: update layout structure

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header.jsx";
-import Footer from "../Footer/Footer.jsx";
 import styles from "./Layout.module.scss";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
